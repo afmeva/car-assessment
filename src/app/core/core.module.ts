f4470f4044bb9c12a49cb300c78a0f4def38588e
afmeva <afmeva@gmail.com>
2019-02-05 21:50:35 -0500

    create navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,12 @@ import { CarsAPIService } from "./services/cars-api.service";
 import { CarBoxComponent } from './components/car-box/car-box.component';
 import { BrowserModule } from "@angular/platform-browser";
 import { CarDetailsComponent } from './components/car-details/car-details.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [CarBoxComponent, CarDetailsComponent],
+  declarations: [CarBoxComponent, CarDetailsComponent, NavbarComponent],
   providers: [CarsAPIService, CarsService],
-  exports: [CarBoxComponent, CarDetailsComponent]
+  exports: [CarBoxComponent, CarDetailsComponent, NavbarComponent]
 })
 export class CoreModule {}
