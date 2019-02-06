b0514338b5a1cb9672748bd6937137f98b82fcfc
afmeva <afmeva@gmail.com>
2019-02-05 22:33:56 -0500

    Add default data for car details

--- src/app/core/components/car-details/car-details.component.ts
+++ src/app/core/components/car-details/car-details.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
-import { Car } from '../../models/car.model';
+import { Component, Input } from "@angular/core";
+import { Car } from "../../models/car.model";
 
 @Component({
-  selector: 'app-car-details',
-  templateUrl: './car-details.component.html',
-  styleUrls: ['./car-details.component.scss']
+  selector: "app-car-details",
+  templateUrl: "./car-details.component.html",
+  styleUrls: ["./car-details.component.scss"]
 })
 export class CarDetailsComponent {
-  @Input() car: Car;
+  @Input() car: Car = {
+    id: "",
+    images: {
+      small: "",
+      big: ""
+    },
+    model: "",
+    year: 0,
+    brand: "",
+    price: 0,
+    features: []
+  };
 }
