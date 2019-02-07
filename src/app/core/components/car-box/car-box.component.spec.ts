398cccceff33eda0f1e941b14de7b5b635669951
afmeva <afmeva@gmail.com>
2019-02-07 11:09:09 -0500

    Add unit tests for car-box

--- src/app/core/components/car-box/car-box.component.spec.ts
+++ src/app/core/components/car-box/car-box.component.spec.ts
@@ -1,18 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CarBoxComponent } from './car-box.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { CarBoxComponent } from "./car-box.component";
+import { RouterTestingModule } from "@angular/router/testing";
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs/Observable";
 
-describe('CarBoxComponent', () => {
+fdescribe("CarBoxComponent", () => {
   let component: CarBoxComponent;
   let fixture: ComponentFixture<CarBoxComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [CarBoxComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule.withRoutes([
+          {
+            path: "details",
+            component: class dummy {}
+          }
+        ])
+      ],
+      declarations: [CarBoxComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: Observable.of({
+              id: ""
+            })
+          }
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,11 +39,43 @@ describe('CarBoxComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it("should emit addToCompare event when buttons are clicked", () => {
+  it("should call onAddToCompare event when add button is clicked", () => {
+    spyOn(component, "onAddToCompare");
 
-  })
+    const addButton = fixture.debugElement.nativeElement.querySelector(
+      "button"
+    );
+    addButton.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.onAddToCompare).toHaveBeenCalled();
+    });
+  });
+
+  it("should call onAddToCompare event when remove button is clicked", () => {
+    spyOn(component, "onAddToCompare");
+
+    component.isAdded = false;
+    fixture.detectChanges();
+
+    const removeButton = fixture.debugElement.nativeElement.querySelector(
+      "button"
+    );
+    removeButton.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.onAddToCompare).toHaveBeenCalled();
+    });
+  });
+
+  it("should emit addToCompare event when onAddToCompare is called", () => {
+    spyOn(component.addToCompare, "emit");
+
+    component.onAddToCompare(new Event(""));
+    expect(component.addToCompare.emit).toHaveBeenCalled();
+  });
 });
